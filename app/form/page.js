e6561feec889c00cfd97826406833c9877a4db3a
yohannesjho <jhonians@outlook.com>
2024-12-02 11:58:01 +0300

    update print button

--- app/form/page.js
+++ app/form/page.js
@@ -142,7 +142,7 @@ export default function FormBuilder() {
             {formStructure.fields.map(renderField)}
           </form>
           <button
-            onClick={handlePrint}
+            onClick={()=>handlePrint()}
             className="bg-blue-500 hover:bg-blue-600 duration-300 text-white px-4 py-2 mt-4 rounded-lg"
           >
             Print Form
