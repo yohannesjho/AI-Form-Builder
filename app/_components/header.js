8aeddfc5a8387b31b3730d68d1dcf16aa583b2e4
yohannesjho <jhonians@outlook.com>
2024-12-02 11:46:20 +0300

    change title name

--- app/_components/header.js
+++ app/_components/header.js
@@ -5,18 +5,20 @@ import React from 'react'
 export default function Header() {
     return (
         <div className='border py-4 px-8'>
-            <SignedOut>
-                <div className='flex justify-between'>
-                    <p>AI Form Builder</p>
-                    <SignInButton />
-                </div>
-            </SignedOut>
-            <SignedIn>
-                <div className='flex justify-between'>
-                    <p>AI Form Builder</p>
-                    <UserButton />
-                </div>
-            </SignedIn>
+            <ClerkProvider>
+                <SignedOut>
+                    <div className='flex justify-between'>
+                        <p>AI Form Builder</p>
+                        <SignInButton />
+                    </div>
+                </SignedOut>
+                <SignedIn>
+                    <div className='flex justify-between'>
+                        <p>AI Form Builder</p>
+                        <UserButton />
+                    </div>
+                </SignedIn>
+            </ClerkProvider>
 
         </div>
     )
